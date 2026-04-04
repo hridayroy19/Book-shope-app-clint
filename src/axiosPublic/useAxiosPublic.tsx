dc298f5a768661e2b4cd2dc95c1894992c113e22
hridayroy19 <hrhridoyroy503@gmail.com>
2026-04-04 23:16:43 +0600

    update

--- src/axiosPublic/useAxiosPublic.tsx
+++ src/axiosPublic/useAxiosPublic.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const axiosPublic = axios.create({ baseURL: "http://localhost:5000/api" });
-const axiosPublic = axios.create({ baseURL: "https://book-shop-projcet.vercel.app/api" });
+const axiosPublic = axios.create({ baseURL: "http://localhost:5000/api" });
+// const axiosPublic = axios.create({ baseURL: "https://book-shop-projcet.vercel.app/api" });
 
 const useAxiosPublic = () => {
   return axiosPublic;
